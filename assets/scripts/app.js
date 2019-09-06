c5eaeb6371affa1b38caf0ab59f193656d013368
elinag <elina.gorshkova@icloud.com>
2019-09-06 09:27:51 -0400

    Initial committ
    Add browser template

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,18 +5,7 @@
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
-const authEvents = require('./auth/events')
-const gameEvents = require('./script')
+
 $(() => {
-  $(document).ready(function () {
-    $('#starting-form').modal('show')
-  })
-  $('#sign-up').on('submit', authEvents.onSignUp)
-  $('#sign-in').on('submit', authEvents.onSignIn)
-  $('#change-password').on('submit', authEvents.onChangePassword)
-  $('#sign-out').on('click', authEvents.onSignOut)
-  $('.new-game').on('click', gameEvents.newGame)
-  $('.cell').on('click', gameEvents.game)
-  $('.cell').on('click', authEvents.onEachMove)
-  $('#check-api').on('click', authEvents.onCheckApi)
+  // your JS code goes here
 })
